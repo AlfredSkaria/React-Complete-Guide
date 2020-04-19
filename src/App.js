06a630c99c7496f3e7d8724d336bd4eebd00eba1
Alfred Skaria <alfiskaria@gmail.com>
2020-04-19 13:09:36 +0530

    removed the commented code.

--- src/App.js
+++ src/App.js
@@ -64,19 +64,6 @@ class App extends Component {
               return <Person name={person.name} 
                              age={person.age}/>
             })}
-            {/* <Person
-              name={this.state.persons[0].name} 
-              age ={this.state.persons[0].age}/>
-            
-            <Person 
-              name={this.state.persons[1].name} 
-              age ={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Anil Skaria')}
-              changed={this.nameChangedHandler}> My Hobbies : racing</Person>
-            
-            <Person 
-              name={this.state.persons[2].name} 
-              age ={this.state.persons[2].age}/> */}
           </div>
         );
     }
