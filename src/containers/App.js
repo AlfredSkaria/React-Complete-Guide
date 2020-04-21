a394d70ca6f370dcbe11a46c62e431ea8aa9b691
Alfred Skaria <alfiskaria@gmail.com>
2020-04-21 22:35:38 +0530

    Restructured the project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 import classes from './App.css'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 // import styled from 'styled-components';
 // import Radium, {StyleRoot} from 'radium';
 
@@ -61,15 +60,13 @@ class App extends Component {
         persons = (
           <div >
             {this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id}> 
-
-                      <Person 
+              return  <Person 
                         click={ () => this.deletePersonHandler(index)}
                         name={person.name} 
                         age={person.age}
                         key={person.id}
                         changed={(event) => this.nameChangedHandler(event, person.id)}/> 
-                    </ErrorBoundary>
+        
             })}
           </div>
         );
