c1899f7681b4f81b753ceff3e138026f0d417528
Alfred Skaria <alfiskaria@gmail.com>
2020-04-22 00:20:03 +0530

    Added the mock implementation of lifcycle hooks for class based components for both props and state changes.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-import classes from './App.css'
-
+import React, { Component } from "react";
+import "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+import classes from "./App.css";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    console.log('[App.js constructos]');
+    console.log("[App.js constructos]");
   }
 
   state = {
     persons: [
-      {id: '100', name: 'Max', age: 28},
-      {id: '101', name: 'Alfi', age: 24},
-      {id: '102', name: 'Alex', age: 22}
+      { id: "100", name: "Max", age: 28 },
+      { id: "101", name: "Alfi", age: 24 },
+      { id: "102", name: "Alex", age: 22 }
     ],
-    otherState: 'Some other value',
+    otherState: "Some other value",
     showPersons: false
   };
 
-  static getDerivedStateFromProps(props,state) { 
-    console.log('[App.js] getDerivedStateFromProps ', props);
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps ", props);
     return state;
   }
 
+  // componentWillMount() {
+  //   console.log("[App.js] componentWillMount");
+  // }
 
-  componentWillMount () {
-    console.log('[App.js] componentWillMount');
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
   }
 
-  componentDidMount(){
-    console.log('[App.js] componentDidMount');
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
-
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
@@ -46,52 +51,52 @@ class App extends Component {
       ...this.state.persons[personIndex]
     };
 
-   // const person = Object.assign({}, this.state.persons[personIndex]);  ///Alternative approach / traditional way
+    // const person = Object.assign({}, this.state.persons[personIndex]);  ///Alternative approach / traditional way
 
     newPerson.name = event.target.value;
 
     const newPersons = [...this.state.persons];
     newPersons[personIndex] = newPerson;
 
-    this.setState({persons: newPersons});
-  }
+    this.setState({ persons: newPersons });
+  };
 
-  deletePersonHandler = (personIndex) => {
-    //const persons = this.state.persons.slice(); 
-    const persons = [...this.state.persons];  // ES6 approach
+  deletePersonHandler = personIndex => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]; // ES6 approach
     let deletedItems = persons.splice(personIndex, 1);
     console.log(deletedItems);
-    this.setState({persons: persons})
-  }
+    this.setState({ persons: persons });
+  };
 
-  togglePersonsHandler = () =>{
-      const doesShow = this.state.showPersons;
-      this.setState({showPersons: !doesShow});
-  }
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-    console.log('[App.js] render');
+    console.log("[App.js] render");
 
     let persons = null;
     if (this.state.showPersons) {
-        persons = ( 
-            <Persons  
-            persons={this.state.persons}
-            clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler}/>
-        );
-
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
     }
     return (
       <div className={classes.App}>
-        <Cockpit  
+        <Cockpit
           title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonsHandler}/>
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
-
     );
 
     //return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Hi, I\'m a react app !!' ))
